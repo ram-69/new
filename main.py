0a93b37c6f42449dc1e164a053df19874b7531cb
ram-69 <105635995+ram-69@users.noreply.github.com>
2022-05-24 00:16:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+
+
+class MainHandler(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write('Hello world!')
+        self.response.out.write("<br />")
+        self.response.out.write('<img src="https://www.w3schools.com/tags/img_girl.jpg" alt="Girl in a jacket" width="500" height="600">                          ')
+        self.response.out.write("<br />")
+        self.response.out.write('#####################')
+
+        even=[]
+        odd=[]
+        n=[]
+        for i in range(100):
+            if(i%2==0):
+                even.append(i)
+            else:
+                odd.append(i)
+
+        self.response.out.write(even)
+        self.response.out.write(odd)
+
+
+def main():
+    application = webapp.WSGIApplication([('/', MainHandler)],
+                                         debug=True)
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
